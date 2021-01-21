dd10f04746e368ea9a3bc5dea245ec32409f660f
Starinin Andrey <starininandrey@gmail.com>
2021-01-21 15:35:44 +0300

    Добавление работы с меню

--- main.cpp
+++ main.cpp
@@ -5,7 +5,36 @@ using namespace std;
 
 int main()
 {
+    system("chcp 65001");
+
+    char select;
+    bool isRun = true;
+
     Welcome();
 
+    do {
+        ShowMenu();
+        cout << "Введите необходимый пункт меню: ";
+        cin >> select;
+        switch (select) {
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '0':
+                isRun = false;
+                break;
+            default:
+                cout << "Вы ввели неправильный пункт меню" << endl;
+                break;
+        }
+    } while (isRun);
 
+    cout << "До встречи..." << endl;
 }
