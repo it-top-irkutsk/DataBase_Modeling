0012cfcf4f15fb15274848db582b69e77545fcbd
LetRuin <77437949+LetRuin@users.noreply.github.com>
2021-01-26 20:16:40 +0800

    Добавление функции преобразования

--- Sex.h
+++ Sex.h
@@ -19,3 +19,13 @@ string SexToString(Sex sex)
             return "другой";
     }
 }
+Sex StringToSex(int number) {
+    switch (number) {
+        case 0:
+            return Male;
+        case 1:
+            return Female;
+        case 2:
+            return Other;
+    }
+}
